806e7c38a052ff070cfd8a984abc3d47f7594c18
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-12-08 17:54:08 +0200

    fix__defaultProps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,17 +9,13 @@ import { Filter } from './Filter/Filter';
 import { Notification } from './Notification/Notification';
 
 export class App extends Component {
-  static defaultProps = {
+  state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-  };
-
-  state = {
-    contacts: this.props.contacts,
     filter: '',
   };
 
