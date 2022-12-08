12acd7ff59191ba469a2724ec7ae49067fe71f99
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-12-08 20:23:23 +0200

    fix

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { ContactListItem, ContactListItemDelete } from './ContactCard.styled';
 
-export const ContactCard = ({ id, name, number, onDelete }) => {
+export const ContactCard = ({ name, number, onDelete }) => {
   return (
     <ContactListItem>
       {name}: {number}
@@ -13,4 +13,5 @@ export const ContactCard = ({ id, name, number, onDelete }) => {
 ContactCard.propTypes = {
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
